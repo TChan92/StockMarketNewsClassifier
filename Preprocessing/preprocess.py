a700899db850a0c2dde7def096618ec4810c4f71
TChan92 <z2x4c6@gmail.com>
2017-06-10 18:35:45 -0700

    Preprocess.py is now wrapped in a class. It will give results with the .get_results() function which returns [pos_data,neg_data]

--- Preprocessing/preprocess.py
+++ Preprocessing/preprocess.py
@@ -7,7 +7,7 @@ import re
 
 
 def check_type(s):
-	if type(data_pos_list[row]) != str:
+	if type(s) != str:
 		return s.encode('ascii','replace')
 	return s
 
@@ -22,37 +22,55 @@ def remove_quotes(s):
 		final += sentence[2:-2]
 	return final
 
-# Grab our data into pandas dataframe
-stemmer = stem.PorterStemmer()
-data = pd.read_csv('../data/Combined_News_DJIA.csv')
-data_pos = data[data['Label'] == 1]
-data_neg = data[data['Label'] == 0]
+class Preprocess():
 
+	def __init__(self):
+		self.pos_data, self.neg_data = self.preprocess()
 
-# Remove the stop words
-stop = stopwords.words('english')
-for line in data_pos:
-	line = [l.lower() for l in wordpunct_tokenize(line) if l.lower() not in stop]
+	def preprocess(self):
+		# Grab our data into pandas dataframe
+		stemmer = stem.PorterStemmer()
+		data = pd.read_csv('../data/Combined_News_DJIA.csv')
+		data_pos = data[data['Label'] == 1]
+		data_neg = data[data['Label'] == 0]
 
-# Put the data into a vectors
-data_pos_list = []
-data_neg_list = []
-for row in range(0, len(data_pos.index)):
-	data_pos_list.append(' '.join(str(x) for x in data_pos.iloc[row,2:27]))
-for row in range(0, len(data_neg.index)):
-	data_neg_list.append(' '.join(str(x) for x in data_neg.iloc[row,2:27]))
+		# Remove the stop words
+		stop = stopwords.words('english')
+		for line in data_pos:
+			line = [l.lower() for l in wordpunct_tokenize(line) if l.lower() not in stop]
 
-# Stem and remove the b'' surrounding every headline
-# Also turns unicode into strings
-for row in range(0, len(data_pos_list)):
-	data_pos_list[row] = stemmer.stem(re.sub('[^A-Za-z0-9.,\'\" ]+', '', data_pos_list[row]))
-	data_pos_list[row] = check_type(data_pos_list[row])
-	data_pos_list[row] = remove_quotes(data_pos_list[row])
-for row in range(0, len(data_neg_list)):
-	data_neg_list[row] = stemmer.stem(re.sub('[^A-Za-z0-9.,\'\" ]+', '', data_neg_list[row]))
-	data_pos_list[row] = check_type(data_pos_list[row])
-	data_neg_list[row] = remove_quotes(data_neg_list[row])
+		# Put the data into a vectors
+		data_pos_list = []
+		data_neg_list = []
+		for row in range(0, len(data_pos.index)):
+			data_pos_list.append(' '.join(str(x) for x in data_pos.iloc[row,2:27]))
+		for row in range(0, len(data_neg.index)):
+			data_neg_list.append(' '.join(str(x) for x in data_neg.iloc[row,2:27]))
 
-vectorizer = CountVectorizer(lowercase=True, stop_words='english',  max_df=1.0, min_df=1, max_features=None,  binary=True, ngram_range=(2,2), token_pattern='[\'\"][a-zA-Z0-9]*[\'\"]')
-pos_data = vectorizer.fit_transform(data_pos_list)
-neg_data = vectorizer.transform(data_neg_list)
+		# Stem and remove the b'' surrounding every headline
+		# Also turns unicode into strings
+		for row in range(0, len(data_pos_list)):
+			data_pos_list[row] = stemmer.stem(re.sub('[^A-Za-z0-9.,\'\" ]+', '', data_pos_list[row]))
+			data_pos_list[row] = check_type(data_pos_list[row])
+			data_pos_list[row] = remove_quotes(data_pos_list[row])
+		for row in range(0, len(data_neg_list)):
+			data_neg_list[row] = stemmer.stem(re.sub('[^A-Za-z0-9.,\'\" ]+', '', data_neg_list[row]))
+			data_pos_list[row] = check_type(data_pos_list[row])
+			data_neg_list[row] = remove_quotes(data_neg_list[row])
+
+		vectorizer = CountVectorizer(lowercase=True, stop_words='english',  max_df=1.0, min_df=1, max_features=None,  binary=True, ngram_range=(2,2), token_pattern='[\'\"][a-zA-Z0-9]*[\'\"]')
+		pos_data = vectorizer.fit_transform(data_pos_list)
+		neg_data = vectorizer.transform(data_neg_list)
+
+		return pos_data, neg_data
+
+	def get_results(self):
+		return [self.pos_data, self.neg_data]
+
+def main():
+	preprocess = Preprocess()
+	results = preprocess.get_results()
+	pass
+
+if __name__=='__main__':
+	main()
