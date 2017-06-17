7715740aef2aa5115806ded733cb9ab9abd5ccbe
John He <username@users.noreply.github.com>
2017-06-17 16:18:35 -0700

    demo code for inputting new head line data

--- StockMarketNewsClassifier.py
+++ StockMarketNewsClassifier.py
@@ -16,3 +16,26 @@ clf = MLPClassifier(hidden_layer_sizes=(90, 80, 70), solver='sgd')
 
 params1 = {"train_x": data_train, "train_y": y_train, "test_x": data_test, "test_y": y_test, "model": clf}
 lr = CL.Classifier(params1)
+
+def demo():
+  # Prompt for input data
+  input_ = ''
+  while True:
+      input_ = raw_input("Enter HeadLine: ")
+      if input_ == '':
+          break
+      else:
+        head_line = input_
+        trans_data = preprocess.transform_data(head_line)
+        c = clf.predict(trans_data)
+        prob = clf.predict_proba(trans_data)
+        result = ''
+
+        if int(c[0]) == 0:
+          result += "Down "
+          result += str(prob[0][0] )    
+        else:
+          result += "Up "
+          result += str(prob[0][1] )    
+
+        print result 
